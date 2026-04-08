a19ca9caabf6248626bafd290ab60625c0a232d1
Keshab Rai <sansongrai@gmail.com>
2026-04-07 22:19:12 -0400

    Basic python

---
+++ kp.py
@@ -0,0 +1,23 @@
+names = ["A","B","Z"]
+print(max(names))
+numbers = [10, 20, 30,40]
+print (numbers [1:4])
+data = [5,10,15,20]
+# find the last element
+print(data[-1])
+# add 25 with list and print
+data.append (25)
+print(data)
+# First 3 elements using slicing
+print(data[0:3])
+# start : end- this is list indixing and slicing where start gets included and end is exxluded 
+names = ["A", "Z", "B"]
+print(max(names))
+# Alaphabetical comparison not numerical for an example A - 65, B-66 and Z- 90
+fruits = ["apple", "banana", "cheryy"]
+print(max(fruits))
+# a -97 ASCII b-98, c-99
+# python compares strg character by chacter from left to right
+words = ["bat", "ball"]
+print(max(words))
+print(min(names))
